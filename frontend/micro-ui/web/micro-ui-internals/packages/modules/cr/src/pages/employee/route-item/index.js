136ddf6b69cf8bea2c3c05f2baa3338e171ae6c2
anzarthoughtripples <anzar@thoughtripples.com>
2022-12-01 12:09:44 +0530

    verify

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/route-item/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/route-item/index.js
@@ -5,10 +5,10 @@ import { useTranslation } from "react-i18next";
 import { ReactComponent as BankIcon } from "../Img/BankIcon.svg";
 import { ReactComponent as FileProtected } from "../Img/FileProtected.svg";
 import CrFlow from "./CrFlow";
-import DeathCrFlow from "./DeathFlow/DeathCrFlow";
+// import DeathCrFlow from "./DeathFlow/DeathCrFlow";
 import SelectStructureType from "../../../pageComponents/SelectStructureType";
 import { newConfig as newConfigCR } from "../../../config/config";
-import { DeathRegistration } from "./DeathFlow/DeathRegistraion";
+// import { DeathRegistration } from "./DeathFlow/DeathRegistraion";
 
 const CrFlowApp = ({ parentUrl }) => {
   const { path } = useRouteMatch();
