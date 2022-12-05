beb87149da7a107fce942426d8c320c1929f0eda
priyamalu <krishnapriya.ikm@kerala.gov.in>
2022-12-05 10:14:57 +0530

    migrationfilechange

--- municipal-services/egov-filemgmnt/src/main/resources/db/migration/main/V20221126143200__fm_alterfordatetype_ddl.sql
+++ municipal-services/egov-filemgmnt/src/main/resources/db/migration/main/V20221126143200__fm_alterfordatetype_ddl.sql
@@ -1,2 +1,2 @@
-Alter table eg_fm_applicantpersonal alter column dateofbirth type bigint;
-Alter table eg_fm_applicantpersonal alter column applicationdate type bigint;
+Alter table eg_fm_applicantpersonal alter column dateofbirth type bigint using dateofbirth::bigint;
+Alter table eg_fm_applicantpersonal alter column applicationdate type bigint using applicationdate::bigint;
