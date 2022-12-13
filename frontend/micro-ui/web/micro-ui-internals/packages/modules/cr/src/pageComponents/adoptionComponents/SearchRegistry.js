5bdff1d188e17761b95001066ae41ca569b3a4cd
shruthy2022 <shruthyks007@gmail.com>
2022-12-13 10:42:34 +0530

    cr

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pageComponents/adoptionComponents/SearchRegistry.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pageComponents/adoptionComponents/SearchRegistry.js
@@ -1,132 +1,224 @@
 import React, { useState } from "react";
-import { FormStep,CardLabel, TextInput, TextArea,Dropdown,DatePicker } from "@egovernments/digit-ui-react-components";
+import { FormStep, CardLabel, TextInput, Dropdown, DatePicker, CheckBox } from "@egovernments/digit-ui-react-components";
+import Timeline from "../../components/CRTimeline";
 import { useTranslation } from "react-i18next";
 
 const SearchRegistry = ({ config, onSelect, userType, formData }) => {
-  const stateId = Digit.ULBService.getStateId();
-  const { t } = useTranslation();
-  let validation = {};
-  const { data: place = {}, isLoad } = Digit.Hooks.tl.useTradeLicenseMDMS(stateId, "TradeLicense", "PlaceOfActivity");
-  const [setPlaceofActivity, setSelectedPlaceofActivity] = useState(formData?.TradeDetails?.setPlaceofActivity);
-  const isEdit = window.location.href.includes("/edit-application/")||window.location.href.includes("renew-trade");
-  const [TradeName, setTradeName] = useState(null);
-  const [CommencementDate, setCommencementDate] = useState();
-  let naturetypecmbvalue =null;
-  let cmbPlace = [];
-  place &&
-  place["TradeLicense"] &&
-  place["TradeLicense"].PlaceOfActivity.map((ob) => {
-        cmbPlace.push(ob);
-    });
+    const stateId = Digit.ULBService.getStateId();
+    
+    const { t } = useTranslation();
+    let validation = {};
+    const { data: Menu } = Digit.Hooks.cr.useCRGenderMDMS(stateId, "common-masters", "GenderType");
+    const [ChildDOB, setChildDOB] = useState(formData?.SearchRegistry?.ChildDOB);
+    const [Gender, selectGender] = useState(formData?.SearchRegistry?.Gender);
+    const [ChildAadharNo, setChildAadharNo] = useState(formData?.SearchRegistry?.ChildAadharNo);
+    const [ChildFirstNameEn, setChildFirstNameEn] = useState(formData?.SearchRegistry?.ChildFirstNameEn);
+    const [ChildMiddleNameEn, setChildMiddleNameEn] = useState(formData?.SearchRegistry?.ChildMiddleNameEn);
+    const [ChildLastNameEn, setChildLastNameEn] = useState(formData?.SearchRegistry?.ChildLastNameEn);
+    const [ChildFirstNameMl, setChildFirstNameMl] = useState(formData?.SearchRegistry?.ChildFirstNameMl);
+    const [ChildMiddleNameMl, setChildMiddleNameMl] = useState(formData?.SearchRegistry?.ChildMiddleNameMl);
+    const [ChildLastNameMl, setChildLastNameMl] = useState(formData?.SearchRegistry?.ChildLastNameMl);
+    const [isAdopted, setIsAdopted] = useState(formData?.SearchRegistry?.isAdopted);
+    const [isMultipleBirth, setIsMultipleBirth] = useState(formData?.SearchRegistry?.isMultipleBirth);
+    const [isFatherInfo, setIsFatherInfo] = useState(formData?.SearchRegistry?.isFatherInfo);
+    const [isMotherInfo, setIsMotherInfo] = useState(formData?.SearchRegistry?.isMotherInfo);
+    const [isBornOutSide, setIsBornOutSide] = useState(formData?.SearchRegistry?.isBornOutSide);
+    const [ChildPassportNo, setChildPassportNo] = useState(formData?.SearchRegistry?.ChildPassportNo);
+    const [ChildArrivalDate, setChildArrivalDate] = useState(formData?.SearchRegistry?.ChildArrivalDate);
 
-  const onSkip = () => onSelect();
+    const isEdit = window.location.href.includes("/edit-application/") || window.location.href.includes("renew-trade");
 
-  function selectPlaceofactivity(value) {
-    naturetypecmbvalue=value.code.substring(0, 4);
-    setSelectedPlaceofActivity(value);    
-  }
-  
-  function setSelectTradeName(e) {
-    setTradeName(e.target.value);
-  }
-  function selectCommencementDate(value) {
-    setCommencementDate(value);
-  }
-  
- 
-  const goNext = () => {
-    sessionStorage.setItem("PlaceOfActivity", setPlaceofActivity.code);   
-    onSelect(config.key, { setPlaceofActivity });
-  }
-  return (
-    <React.Fragment>
-    {window.location.href.includes("/citizen") ? <Timeline /> : null}
-    <FormStep t={t} config={config} onSelect={goNext} onSkip={onSkip} isDisabled={!CommencementDate}>
-    <header className="card-header" style={{fontSize:"35px"}}>Public Place</header>
-    <div className="row">
-        <div className="col-md-12" ><h1 className="headingh1" ><span style={{ background: "#fff", padding: "0 10px" }}>{`${t("Public Place")}`}</span> </h1>
- </div>
-   </div> 
-   
-    <div className="row">
-         <div className="col-md-4" >
-            <CardLabel>{`${t("Type")}`}</CardLabel>
-            <Dropdown
-                t={t}
-                optionKey="code"
-                isMandatory={false}
-                option={cmbPlace}
-                selected={setPlaceofActivity}
-                select={selectPlaceofactivity}
-                disabled={isEdit}
-            />
-        </div>
-        <div className="col-md-4" >
-            <CardLabel>{`${t("CR_INFORMANT_NAME")}`}</CardLabel>
-            <TextInput       
-            t={t}
-            isMandatory={false}
-            type={"text"}
-            optionKey="i18nKey"
-            name="TradeName"
-            value={TradeName}
-            onChange={setSelectTradeName}
-            disable={isEdit}
-            {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })}
-            />
-        </div>
-        <div className="col-md-4" >
-            <CardLabel>{`${t("CS_COMMON_WARD")}`}</CardLabel>
-            <Dropdown
-                t={t}
-                optionKey="code"
-                isMandatory={false}
-                option={cmbPlace}
-                selected={setPlaceofActivity}
-                select={selectPlaceofactivity}
-                disabled={isEdit}
-            />
-        </div> 
+    let menu = [];
+    Menu &&
+        Menu.map((genderDetails) => {
+            menu.push({ i18nKey: `CR_COMMON_GENDER_${genderDetails.code}`, code: `${genderDetails.code}`, value: `${genderDetails.code}` });
+        });
 
-    </div>
-    <div className="row">    
-        <div className="col-md-12" >
-            <CardLabel>{`${t("CR_ADDRESS")}`}</CardLabel>
-            <TextArea       
-            t={t}
-            isMandatory={false}
-            type={"text"}
-            optionKey="i18nKey"
-            name="TradeName"
-            value={TradeName}
-            onChange={setSelectTradeName}
-            disable={isEdit}
-            {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })}
-            />
-        </div>
-      
-    </div>
-    <div className="row">    
-         
-        <div className="col-md-12" >
-            <CardLabel>{`${t("CR_DESCRIPTION")}`}</CardLabel>
-            <TextArea       
-            t={t}
-            isMandatory={false}
-            type={"text"}
-            optionKey="i18nKey"
-            name="TradeName"
-            value={TradeName}
-            onChange={setSelectTradeName}
-            disable={isEdit}
-            {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })}
-            />
-        </div>    
-    </div>
-      
-    </FormStep>
-    </React.Fragment>
-  );
+    const onSkip = () => onSelect();
+
+    function setselectGender(value) {
+        selectGender(value);
+    }
+    function setSelectChildAadharNo(e) {
+        setChildAadharNo(e.target.value);
+    }
+    function setselectChildDOB(value) {
+        setChildDOB(value);
+    }
+    function setSelectChildFirstNameEn(e) {
+        setChildFirstNameEn(e.target.value);
+    }
+    function setSelectChildMiddleNameEn(e) {
+        setChildMiddleNameEn(e.target.value);
+    }
+    function setSelectChildLastNameEn(e) {
+        setChildLastNameEn(e.target.value);
+    }
+    function setSelectChildFirstNameMl(e) {
+        setChildFirstNameMl(e.target.value);
+    }
+    function setSelectChildMiddleNameMl(e) {
+        setChildMiddleNameMl(e.target.value);
+    }
+    function setSelectChildLastNameMl(e) {
+        setChildLastNameMl(e.target.value);
+    }
+    function setAdopted(e) {
+        if (e.target.checked == true) {
+            setIsAdopted(true);
+        } else {
+            setIsAdopted(false);
+        }
+    }
+    function setMultipleBirth(e) {
+        if (e.target.checked == true) {
+            setIsMultipleBirth(true);
+        } else {
+            setIsMultipleBirth(false);
+        }
+    }
+    function setFatherInfo(e) {
+        if (e.target.checked == true) {
+            setIsFatherInfo(true);
+        } else {
+            setIsFatherInfo(false);
+        }
+    }
+    function setMotherInfo(e) {
+        if (e.target.checked == true) {
+            setIsMotherInfo(true);
+        } else {
+            setIsMotherInfo(false);
+        }
+    }
+    function setBornOutSide(e) {
+        console.log(e.target.checked);
+        if (e.target.checked === true) {
+            
+            setIsBornOutSide(true);
+            console.log(isBornOutSide);
+
+        } else {
+            setIsBornOutSide(false);
+        }
+        
+    }
+    function setSelectPassportNo(e) {
+        setChildPassportNo(e.target.value);
+    }
+    function setSelectArrivalDate(e) {
+        setChildArrivalDate(e.target.value);
+    }
+
+    const goNext = () => {
+        sessionStorage.setItem("ChildDOB", ChildDOB);
+        sessionStorage.setItem("Gender", Gender.code);
+        sessionStorage.setItem("ChildAadharNo", ChildAadharNo);
+        sessionStorage.setItem("ChildFirstNameEn", ChildFirstNameEn);
+        sessionStorage.setItem("ChildMiddleNameEn", ChildMiddleNameEn);
+        sessionStorage.setItem("ChildLastNameEn", ChildLastNameEn);
+        sessionStorage.setItem("ChildFirstNameMl", ChildFirstNameMl);
+        sessionStorage.setItem("ChildMiddleNameMl", ChildMiddleNameMl);
+        sessionStorage.setItem("ChildLastNameMl", ChildLastNameMl);
+        sessionStorage.setItem("isAdopted", isAdopted);
+        sessionStorage.setItem("isMultipleBirth", isMultipleBirth);
+        sessionStorage.setItem("isFatherInfo", isFatherInfo);
+        sessionStorage.setItem("isMotherInfo", isMotherInfo);
+        sessionStorage.setItem("isBornOutSide", isBornOutSide);
+        sessionStorage.setItem("ChildPassportNo", ChildPassportNo);
+        sessionStorage.setItem("ChildArrivalDate", ChildArrivalDate);
+        onSelect(config.key, { ChildDOB, Gender, ChildAadharNo, ChildFirstNameEn, ChildMiddleNameEn, ChildLastNameEn, ChildFirstNameMl, ChildMiddleNameMl, ChildLastNameMl, isAdopted, isMultipleBirth, isFatherInfo, isMotherInfo, isBornOutSide, ChildPassportNo, ChildArrivalDate });
+    }
+    return (
+        <React.Fragment>
+            {window.location.href.includes("/citizen") ? <Timeline  /> : null}
+            {window.location.href.includes("/employee") ? <Timeline /> : null}
+            <FormStep t={t} config={config} onSelect={goNext} onSkip={onSkip} isDisabled={!ChildDOB}>
+
+                <div className="row">
+                    <div className="col-md-12" ><h1 className="headingh1" ><span style={{ background: "#fff", padding: "0 10px" }}>{`${t("Registration Details")}`}</span> </h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4" ><CardLabel>{t("CR_DATE_OF_BIRTH_TIME")}</CardLabel>
+                        <DatePicker date={ChildDOB} name="ChildDOB" onChange={setselectChildDOB} placeholder={`${t("CR_DATE_OF_BIRTH_TIME")}`} />
+                    </div>
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_GENDER")}`}</CardLabel>
+                        <Dropdown t={t} optionKey="code" isMandatory={true} option={menu} selected={Gender} select={setselectGender} disabled={isEdit} placeholder={`${t("CR_GENDER")}`} {...(validation = { isRequired: true, title: t("TL_INVALID_GENDER") })} />
+                    </div>
+                    <div className="col-md-4"> <CardLabel>{`${t("Aadhar No")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildAadharNo" value={ChildAadharNo} onChange={setSelectChildAadharNo} disable={isEdit} placeholder={`${t("Aadhar No")}`} {...(validation = { pattern: "^([0-9]){12}$", isRequired: true, type: "text", title: t("TL_INVALID_AADHAR_NO") })} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12" ><h1 className="headingh1" ><span style={{ background: "#fff", padding: "0 10px" }}>{`${t("Name of Child")}`}</span> </h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_FIRST_NAME")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildFirstNameEn" value={ChildFirstNameEn} onChange={setSelectChildFirstNameEn} disable={isEdit} placeholder={`${t("CR_FIRST_NAME")}`} {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                    </div>
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_MIDDLE_NAME")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildMiddleNameEn" value={ChildMiddleNameEn} onChange={setSelectChildMiddleNameEn} disable={isEdit} placeholder={`${t("CR_MIDDLE_NAME")}`} {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: false, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                    </div>
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_LAST_NAME")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildLastNameEn" value={ChildLastNameEn} onChange={setSelectChildLastNameEn} disable={isEdit} placeholder={`${t("CR_LAST_NAME")}`} {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_FIRST_NAME")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildFirstNameMl" value={ChildFirstNameMl} onChange={setSelectChildFirstNameMl} disable={isEdit} placeholder={`${t("CR_FIRST_NAME")}`} {...(validation = { isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                    </div>
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_MIDDLE_NAME")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildMiddleNameMl" value={ChildMiddleNameMl} onChange={setSelectChildMiddleNameMl} disable={isEdit} placeholder={`${t("CR_MIDDLE_NAME")}`} {...(validation = { isRequired: false, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                    </div>
+                    <div className="col-md-4" > <CardLabel>{`${t("CR_LAST_NAME")}`}</CardLabel>
+                        <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildLastNameMl" value={ChildLastNameMl} onChange={setSelectChildLastNameMl} disable={isEdit} placeholder={`${t("CR_LAST_NAME")}`} {...(validation = { isRequired: true, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12" ><h1 className="headingh1" ><span style={{ background: "#fff", padding: "0 10px" }}>{`${t("Other Details")}`}</span> </h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6" >
+                        {/* <CardLabel>{`${t("CR_GENDER")}`}</CardLabel> */}
+                        <CheckBox label={t("Adopted")} onChange={setAdopted} value={isAdopted} checked={isAdopted} />
+                    </div>
+                    <div className="col-md-6" >
+                        {/* <CardLabel>{`${t("Multiple Birth")}`}</CardLabel> */}
+                        <CheckBox label={t("Multiple Birth")} onChange={setMultipleBirth} value={isMultipleBirth} checked={isMultipleBirth} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6" >
+                        {/* <CardLabel>{`${t("CR_GENDER")}`}</CardLabel> */}
+                        <CheckBox label={t("Father Information Missing")} onChange={setFatherInfo} value={isFatherInfo} checked={isFatherInfo} />
+                    </div>
+                    <div className="col-md-6" >
+                        {/* <CardLabel>{`${t("CR_GENDER")}`}</CardLabel> */}
+                        <CheckBox label={t("Mother Information Missing")} onChange={setMotherInfo} value={isMotherInfo} checked={isMotherInfo} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12" >
+                        <CheckBox label={t("Born OutSide India")} onChange={setBornOutSide} value={isBornOutSide} checked={isBornOutSide} />
+                    </div>
+                </div>
+                {isBornOutSide === "true" && (
+                    <div>
+                        <div className="row">
+                            <div className="col-md-6" > <CardLabel>{`${t("Passport No")}`}</CardLabel>
+                                <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildPassportNo" value={ChildPassportNo} onChange={setSelectPassportNo} disable={isEdit} placeholder={`${t("Passport No")}`} {...(validation = { pattern: "^[a-zA-Z-.0-9`' ]*$", isRequired: false, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                            </div>
+                            <div className="col-md-6" > <CardLabel>{`${t("Date of Arrival")}`}</CardLabel>
+                                <TextInput t={t} isMandatory={false} type={"text"} optionKey="i18nKey" name="ChildArrivalDate" value={ChildArrivalDate} onChange={setSelectArrivalDate} disable={isEdit} placeholder={`${t("Date of Arrival")}`} {...(validation = { pattern: "^[a-zA-Z-.`' ]*$", isRequired: false, type: "text", title: t("TL_INVALID_TRADE_NAME") })} />
+                            </div>
+                        </div>
+                    </div>)}
+
+            </FormStep>
+        </React.Fragment>
+    );
 };
-
 export default SearchRegistry;
