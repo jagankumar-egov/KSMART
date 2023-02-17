79706e14e95dddb22e9929b28e896ca2017aab97
ikmkerala <107991882+ikmkerala@users.noreply.github.com>
2023-02-17 17:33:25 +0530

    Update PGRConstants.java

--- municipal-services/pgr-services/src/main/java/org/egov/pgr/util/PGRConstants.java
+++ municipal-services/pgr-services/src/main/java/org/egov/pgr/util/PGRConstants.java
@@ -21,9 +21,7 @@ public class PGRConstants {
     public static final String PGR_SERVICE_ENGG_DEPT = "PWD";
 
     public static final String PGR_SERVICE_HEALTH_DEPT = "HTH";
-   
-
-
+  
     public static final String USERTYPE_EMPLOYEE = "EMPLOYEE";
    
     public static final String USERTYPE_CITIZEN = "CITIZEN";
